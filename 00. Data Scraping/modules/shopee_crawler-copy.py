6a341a3dd40d093e104e81ea7cc066d59e7fc3f7
Manh Cuong <cuongpigerr@gmail.com>
2021-05-29 18:36:43 +0700

    .

--- 00. Data Scraping/modules/shopee_crawler-copy.py
+++ 00. Data Scraping/modules/shopee_crawler-copy.py
@@ -94,47 +94,72 @@ def getAllRevirewsOfProduct(pUrl):
                 if flag is not None:
                     reviews = driver.find_elements_by_css_selector("div.shopee-product-rating")
                     
-                    if type(reviews) == list and len(reviews) != 0:                        
+                    if type(reviews) == list:
                         for review in reviews:
                             comment = review.find_element_by_class_name("shopee-product-rating__content").text
                             rating = len(review.find_elements_by_class_name("icon-rating-solid--active"))
                             
                             new_review = Review(comment, rating)
                             customers_reviews.append(new_review)
-                    else:
-                        stop = True
                     
                     flag1 = None
-                    try:
-                        flag1 = wait.until(EC.element_to_be_clickable(locator_button))
+                    cnt = 0
+                    while True:
+                        flag1 = None
                         
-                        if flag1 is not None:
-                            flag1.click()
-                    except TimeoutException as e:
-                        stop = True
+                        if cnt > 5: 
+                            stop = True
+                            break
+                        
+                        try:
+                            flag1 = wait.until(EC.element_to_be_clickable(locator_button))
+                            
+                            if flag1 is not None:
+                                flag1.click()
+                        except TimeoutException as e:
+                            cnt += 1
+                            continue
+                        finally:
+                            break
                        
-                    try:
-                        flag2 = wait.until(EC.presence_of_element_located(locator_button_focus))
+                    cnt2 = 0 
+                    while flag1 is not None:
+                        if cnt2 > 5: break
+                        flag2 = None
+                        try:
+                            flag2 = wait.until(EC.presence_of_element_located(locator_button_focus))
+                            
+                            if flag2 is not None:
+                                key = flag2.text.strip()
+                                buttons_box[key] = buttons_box.get(key, 0) + 1
+                                
+                                if buttons_box.get(key) > 1:
+                                    driver.close()
+                                    driver.quit()
+                                    stop = True
+                                    break
+                                
+                        except TimeoutException as e:
+                            cnt2 += 1
+                            continue
+                        finally:
+                            break
                         
-                        if flag2 is not None:
-                            key = flag2.text.strip()
-                            buttons_box[key] = buttons_box.get(key, 0) + 1
-                            
-                            if buttons_box.get(key) > 1:
-                                stop = True
-                            
-                    except TimeoutException as e:
-                        stop = True
-                         
-                try:
-                    flag = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.shopee-product-rating")))
-                except TimeoutException as e:
-                    stop = True
-                    
-                if stop: 
-                    driver.close()
-                    driver.quit()
-                    break
+                if stop: break
                 
+                flag = None
+                
+                cnt3 = 0
+                while flag is None:
+                    if cnt3 > 5:
+                        stop = True
+                        break
+                    
+                    try:
+                        flag = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.shopee-product-rating")))
+                    except TimeoutException as e:
+                        flag = None
+                        cnt3 += 1
+                    finally: break
                 
             return customers_reviews
