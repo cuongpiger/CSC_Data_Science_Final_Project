8dff1440407d94320eb92edce0ac614dd9d07e7c
Manh Cuong <cuongpigerr@gmail.com>
2021-06-02 23:17:27 +0700

    .

--- 01. Data Preprocessing/modules/utils.py
+++ 01. Data Preprocessing/modules/utils.py
@@ -1,9 +1,11 @@
+from __future__ import unicode_literals
 import emojis
 import re
 from os import listdir
 from os.path import isfile, join
 
 detect_url = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+remove_special_characters = r"[^a-z àáâãèéêìíòóôõùúăđĩũơưăạảấầẩẫậắằẳẵặẹẻẽềềểễệỉịọỏốồổỗộớờởỡợụủứừửữựỳỵỷỹ]"
 
 def extract_emoji(pText):
     return list(emojis.get(pText))
@@ -26,3 +28,41 @@ def containsURL(pText: str):
 
 def getFiles(pDirectory):
     return [f for f in listdir(pDirectory) if isfile(join(pDirectory, f))]
+
+def removeSpecialCharacters(pText):
+    return re.sub("\s+", " ", re.sub(remove_special_characters, " ", pText)).strip() 
+
+def removeDupplicateLetters(pText):
+    words = pText.split(" ")
+    
+    for i, word in enumerate(words):
+        words[i] = re.sub(r'(.)\1+', r'\1', word)
+        
+    return " ".join(words)
+
+def removeGibbish(pDictionary, pText):
+    words = []
+    
+    for word in pText.split(" "):
+        if pDictionary.get(word) is not None:
+            words.append(word)
+            
+    return " ".join(words)
+
+
+def fixAcronymWords(pDictionary, pText):
+    words = []
+    
+    for word in pText.split(" "):
+        acronym = pDictionary.get(word)
+        
+        if acronym is None:
+            words.append(word)
+        else:
+            words.append(acronym)
+            
+    return " ".join(words)
+
+def combineCommentAnnEmoji(pText, pEmoji):
+    emoji_text = " ".join(pEmoji)
+    return pText + " " + emoji_text
