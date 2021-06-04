7c72e7a8fcf43a091992c345e8374f7a88037500
Manh Cuong <cuongpigerr@gmail.com>
2021-06-04 12:42:15 +0700

    .

--- 01. Data Preprocessing/modules/regex_patterns.py
+++ 01. Data Preprocessing/modules/regex_patterns.py
@@ -1,3 +1,12 @@
 url_pattern = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
-vietnamese_letters_pattern = r"[^a-z áàảãạâấầẩẫậăắằẳẵặéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵ]"
+vietnamese_letters_pattern = r"[^a-z áàảãạâấầẩẫậăắằẳẵặéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵđ]"
+
+vietnamese_letters_pattern_no_space = r"[^a-záàảãạâấầẩẫậăắằẳẵặéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵđ]"
+
+reviews_paths = [
+    "../00. Data Scraping/data/shopee_reviews/men/",
+    "../00. Data Scraping/data/shopee_reviews/women/",
+    "../00. Data Scraping/data/shopee_reviews/more/",
+    "../00. Data Scraping/data/shopee_reviews/mor/"
+]
--- 01. Data Preprocessing/modules/utils.py
+++ 01. Data Preprocessing/modules/utils.py
@@ -1,42 +1,102 @@
 from __future__ import unicode_literals
 import emojis
 import re
+import numpy as np
+from .regex_patterns import vietnamese_letters_pattern, vietnamese_letters_pattern_no_space, url_pattern
 from os import listdir
 from os.path import isfile, join
 
 
-def extract_emoji(pText):
+def labelRating(pRating: int):
+    if pRating <= 2: return -1
+    
+    if pRating <= 4: return 0
+    
+    return 1
+
+
+def extractEmoji(pText: str):
     return " ".join(list(emojis.get(pText)))
 
-def get_text(pText: str):
-    test = pText.lower()
     
 def containsURL(pText: str):
-    """ Kiểm tra pText có chứa url hay ko
-
-    Args:
-        pText ([type]): [description]
-
-    Returns:
-        (bool): True nếu có và ngc lại
-    """
-    flag = re.search(detect_url, pText)
+    flag = re.search(url_pattern, pText)
     
     return flag is not None
 
-def getFiles(pDirectory):
-    return [f for f in listdir(pDirectory) if isfile(join(pDirectory, f))]
 
-def removeSpecialCharacters(pText):
-    return re.sub("\s+", " ", re.sub(remove_special_characters, " ", pText)).strip() 
-
-def removeDupplicateLetters(pText):
+def removeDuplicateLetters(pText: str):
     words = pText.split(" ")
     
     for i, word in enumerate(words):
         words[i] = re.sub(r'(.)\1+', r'\1', word)
         
     return " ".join(words)
+
+
+def loadVietnameseAbbreviate():
+    vietnamese_abbreviate = {}
+    
+    with open("./modules/dependencies/vietnamese-abbreviate.txt", "r", encoding="utf-8") as reader:
+        items = reader.read().split("\n")
+        for item in items:
+            abbreviate, word = item.split("=")
+            vietnamese_abbreviate[abbreviate] = word
+            
+    return vietnamese_abbreviate
+
+
+def standardVietnameseAbbreviate(pVietnameseAbbreviate, pText):
+    for abbreviate, word in pVietnameseAbbreviate.items():
+        pText = re.sub(f"{vietnamese_letters_pattern_no_space}+{abbreviate}{vietnamese_letters_pattern_no_space}+", f" {word} ", pText)
+
+    return pText.strip()
+
+
+def removeNotVietnameseLetters(pText: str):
+    return re.sub("\s+", " ", re.sub(vietnamese_letters_pattern, " ", pText)).strip() 
+
+
+def loadVietnameseSyllables():
+    vietnamese_syllables = {}
+    
+    with open("./modules/dependencies/vietnamese-syllables.txt", "r", encoding="utf-8") as reader:
+        words = reader.read().split("\n")
+        for word in words:
+            vietnamese_syllables[word] = True
+            
+    return vietnamese_syllables
+
+def loadBoostWords():
+    boost_words = {}
+    
+    with open("./modules/dependencies/boost-words.txt", "r", encoding="utf-8") as reader:
+        words = reader.read().split("\n")
+        for word in words:
+            boost_words[word] = True
+            
+    return boost_words
+
+
+def extractBoostWords(pBoostWords, pText):
+    lst_words = []
+
+    for word in pBoostWords.keys():
+        if re.search(",", word):
+            words = word.split(',')
+            dash_word = None
+            
+            for split_word in words:
+                if re.search(f"{vietnamese_letters_pattern_no_space}+{split_word}{vietnamese_letters_pattern_no_space}+", pText):
+                    dash_word = "_".join(split_word.split(" "))
+                    
+            if dash_word:
+                lst_words.append(dash_word)
+        elif re.search(f"{vietnamese_letters_pattern_no_space}+{word}{vietnamese_letters_pattern_no_space}+", pText):
+            dash_word = "_".join(word.split(" "))
+            lst_words.append(dash_word)
+            
+    return " ".join(lst_words).strip()
 
 def removeGibbish(pDictionary, pText):
     words = []
@@ -46,6 +106,21 @@ def removeGibbish(pDictionary, pText):
             words.append(word)
             
     return " ".join(words)
+
+def combineCommentAndEmoji(pText, pEmoji):
+    if pEmoji == "nan" or pEmoji == "" or pEmoji == np.nan or pEmoji == None:
+        pEmoji = ""
+        
+    return (pText + " " + pEmoji).strip()
+
+
+def getFiles(pDirectory):
+    return [f for f in listdir(pDirectory) if isfile(join(pDirectory, f))]
+
+
+
+
+
 
 
 def fixAcronymWords(pDictionary, pText):
@@ -60,9 +135,3 @@ def fixAcronymWords(pDictionary, pText):
             words.append(acronym)
             
     return " ".join(words)
-
-def combineCommentAndEmoji(pText, pEmoji):
-    if pEmoji == "nan":
-        pEmoji = ""
-        
-    return (pText + " " + pEmoji).strip()
